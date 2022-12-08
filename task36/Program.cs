fb9c5df258b779364a1cbdd4c45cfca3fe71ae27
Nataly <natalya-kotenkova@yandex.ru>
2022-12-08 14:53:02 +0100

    added task36

---
+++ task36/Program.cs
@@ -0,0 +1,21 @@
+﻿void InputArray(int[] array) 
+{ 
+   for (int i = 0; i < array.Length; i++) 
+   array[i]  = new Random().Next(100, 1000);
+} 
+int ReleaseArray(int[] array)
+{ 
+   int sum = 0; 
+   for (int i = 1; i < array.Length; i+=2) 
+   { 
+     sum += array[i] ; 
+   } 
+   return sum;
+} 
+Console.Clear(); 
+Console.Write("Введите кол-во элементов массива: "); 
+int n = Convert.ToInt32(Console.ReadLine()); 
+int[] array = new int[n]; 
+InputArray(array); 
+Console.WriteLine($"Массив: [{string.Join(", ", array)}]"); 
+Console.WriteLine($"{ReleaseArray(array)}"); 
